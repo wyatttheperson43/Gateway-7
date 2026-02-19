b9c93a2a969b83a4852a11cb808c9148da21b16e
wyatttheperson43 <wyattmiller1245@gmail.com>
2026-02-19 17:34:54 -0500

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,9 @@
+function show(game){
+
+document.getElementById("snake").style.display="none";
+document.getElementById("clicker").style.display="none";
+document.getElementById("reaction").style.display="none";
+
+document.getElementById(game).style.display="block";
+
+}
